39f328d741f28d210687735241e87e2e291b194e
xZwei <xphantomzwei@gmail.com>
2022-07-11 16:00:25 -0400

    add material ui

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'prog-angular-intro';
+
+export class AppComponent implements OnInit {
+  readonly title = 'Pokedex';
+  readonly author = 'Wesley Cole';
+
+  constructor() { }
+
+  ngOnInit(): void {
+    return;
+  }
 }
